d20b83da3a975bb4744ba2e13d0c69c6d3795e14
Luca Orioli <lucaorioli@gmail.com>
2024-04-08 20:20:15 +0200

    Update x11vnc-session.sh
    Password added

--- assets/x11vnc-session.sh
+++ assets/x11vnc-session.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-/usr/bin/x11vnc -xkb -noxrecord -noxfixes -noxdamage -display :0 -repeat -nopw -wait 5 -permitfiletransfer -tightfilexfer -auth /var/run/slim.auth
+/usr/bin/x11vnc -xkb -noxrecord -noxfixes -noxdamage -display :0 -repeat -passwd N^%Hv2&9c8@Kp -wait 5 -permitfiletransfer -tightfilexfer -auth /var/run/slim.auth
 #/usr/bin/supervisorctl restart slim
 
 
